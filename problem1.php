474f338f3cad3b4febba56097c60897328051729
Pau <141435859+pauleenann@users.noreply.github.com>
2024-03-22 21:27:32 +0800

    prob1

---
+++ problem1.php
@@ -0,0 +1,14 @@
+<!--Write a PHP script to display the following strings.
+Sample String :
+'Tomorrow I \'ll learn PHP global variables.'
+'This is a bad command : del c:\\*.*'
+Expected Output :
+Tomorrow I 'll learn PHP global variables.
+This is a bad command : del c:\*.*   -->
+
+
+
+<?php
+    echo "Tomorrow I \'ll learn PHP global variables. <br>";
+    echo "This is a bad command : del c:\\*.*";
+?>
